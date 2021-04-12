9cb900eb8dfcbb031337756f6e9a0517e3a34541
OanaMG <oana_ghidiiu@yahoo.com>
2021-04-12 14:41:30 +0100

    edit model

--- Models/DailyEntry.cs
+++ Models/DailyEntry.cs
@@ -3,6 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 public class DailyEntry
 {
+
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
@@ -10,8 +11,10 @@ public class DailyEntry
     public string Topics { get; set; }
     public string NotionLinks { get; set; }
     public string AdditionalResourcesLinks { get; set; }
+    public string GithubLinks { get; set; }
     public string AdditionalNotes { get; set; }
     public int RecapQuizScore { get; set;}
     public string Token { get; set; }
+    public string[] UploadedDocuments { get; set; }           
 
 }
